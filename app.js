185ef94d74b96f033980cfcb894fd6aead2d58ea
Bre01cc <boliveiraassisreis@gmail.com>
2025-08-10 21:02:08 -0300

    quase concluido

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
-const trocarDeImagem = document.getElementById('img')
+const trocarDeImagem = document.getElementById('trocar-img')
+const autoImagem = document.getElementById('auto-img')
 
-function trocarImagem(){
-    
+function trocarImagem() {
+    //pega o nome digitado na caixa de texto atrvés do id'nomeImg'
+    const img = document.getElementById('nomeImg').value
+    //let ordem = ''
+
+    if (img == "1") {
+        let valor = 'celso'
+        const varImg = img
+        const foto = `/img/${varImg}.jpeg`
+        document.documentElement.style.setProperty('--bg-img', `url(${foto})`)
+    }
+    else if (img == "2") {
+        let valor = 'estilo'
+        const varImg = valor
+        const foto = `/img/${varImg}.jpeg`
+        document.documentElement.style.setProperty('--bg-img', `url(${foto})`)
+
+    } else if (img == "3"||img=="java") {
+        let valor = img
+        const varImg = valor
+        const foto = `/img/${varImg}.webp`
+        document.documentElement.style.setProperty('--bg-img', `url(${foto})`)
+
+    } else if (img == "4") {
+        let valor = 'oculos'
+        const varImg = valor
+        const foto = `/img/${varImg}.jpeg`
+        document.documentElement.style.setProperty('--bg-img', `url(${foto})`)
+
+    } else if (img == "5") {
+        let valor = 'pedra'
+        const varImg = valor
+        const foto = `/img/${varImg}.jpeg`
+        document.documentElement.style.setProperty('--bg-img', `url(${foto})`)
+
+    } else {
+        const varImg = img
+        const caminhaImg = `/img/${varImg}.jpeg`
+        document.documentElement.style.setProperty('--bg-img', `url(${caminhaImg})`)
+
+    }
 }
+function auto(){
+    document.documentElement.style.setProperty('--bg-animation', 'slider 12s infinite, fade 4s')
+}
+autoImagem.addEventListener('click', auto)
+trocarDeImagem.addEventListener('click', trocarImagem)
